4d61e1518bec84e0f8f13091bf6847bd13735078
romainsame <romainsame@gmail.com>
2020-11-11 19:17:27 +0100

    commit signin et signup 19:17

--- gestionlicence/urls.py
+++ gestionlicence/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',home)
+    path('',home),
+    path('signin', signin, name="signin"),
+    path('signup', signup, name="signup"),
 ]
--- gestionlicence/views.py
+++ gestionlicence/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'account/signin.html')
+
+def signup(request):
+    return render(request, 'account/signup.html')
