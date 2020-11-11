d6a1a44daf14ec99aeaa569e422c6055c498808e
romainsame <romainsame@gmail.com>
2020-11-11 16:09:29 +0100

    save models admin 11/11/2020  16:09

--- gestionlicence/admin.py
+++ gestionlicence/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Personne)
+admin.site.register(Licence)
+admin.site.register(Application)
+admin.site.register(DeviceType)
+admin.site.register(Package)
