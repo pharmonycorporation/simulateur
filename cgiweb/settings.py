4f6a36f5343775dbdb167a9f51bac5484c18eb63
Kenmogne <kenmognethimotee@gmail.com>
2020-11-30 19:39:26 +0100

    test2

--- cgiweb/settings.py
+++ cgiweb/settings.py
@@ -154,7 +154,7 @@ PAYPAL_TEST = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
-EMAIL_USE_TLS = True
+#EMAIL_USE_TLS = True
 EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
 EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '') 
