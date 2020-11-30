43f6a4c3341037a2db67bce39494e0dca58ce9e1
Kenmogne <kenmognethimotee@gmail.com>
2020-11-30 14:48:53 +0100

    test

--- cgiweb/settings.py
+++ cgiweb/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'gestionlicence',
     'widget_tweaks',
     'paypal.standard.ipn',
-    'whitenoise'
+    'whitenoise',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
