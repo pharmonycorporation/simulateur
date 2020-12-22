1d287d56c5dd074b74b4ab1aa0f4a0f8ee2daa03
Guy Arnaud <mbcode.dev@gmail.com>
2020-12-22 09:55:16 +0100

    finalisation API

--- gendsf/serializers.py
+++ gendsf/serializers.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .models import DSF, DADS, ServiceConseil
+from .models import DSF, DADS, ServiceConseil, Pays, FormeJuridique,EtatSalaire, Service, InformationsAutres,FicheVersementSpontaneIRPP, BalanceSixColonneSYSCohada, FicheVersementAccompteIS, FicheVerSementTVA,Dirigeants,FicheEffectif,MembreConseil, FicheFiscale,ControleEntreprise, Identification, RegimeFiscale
 from rest_framework import routers, serializers, viewsets
 
 class DSFSerializer(serializers.ModelSerializer):
@@ -16,4 +16,84 @@ class DADSSerializer (serializers.ModelSerializer):
 class ServiceConseilSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceConseil
+        fields = '__all__'
+
+class PaysSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pays
+        fields = '__all__'
+
+class FormeJuridiqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FormeJuridique
+        fields = '__all__'
+
+class ControleEntrepriseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ControleEntreprise
+        fields = '__all__'
+
+class IdentificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Identification
+        fields = '__all__'
+
+class RegimeFiscaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RegimeFiscale
+        fields = '__all__'
+
+class FicheFiscaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FicheFiscale
+        fields = '__all__'
+
+class DirigeantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dirigeants
+        fields = '__all__'
+
+class MembreConseilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MembreConseil
+        fields = '__all__'
+
+class FicheEffectifSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FicheEffectif
+        fields = '__all__'
+
+class FicheVerSementTVASerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FicheVerSementTVA
+        fields = '__all__'
+
+class FicheVersementAccompteISSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FicheVersementAccompteIS
+        fields = '__all__'
+
+class BalanceSixColonneSYSCohadaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BalanceSixColonneSYSCohada
+        fields = '__all__'
+
+class InformationsAutresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InformationsAutres
+        fields = '__all__'
+
+class FicheVersementSpontaneIRPPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FicheVersementSpontaneIRPP
+        fields = '__all__'
+
+class EtatSalaireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EtatSalaire
+        fields = '__all__'
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
         fields = '__all__'
--- gendsf/views.py
+++ gendsf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .serializers import ServiceConseilSerializer, DSFSerializer, DADSSerializer
-from .models import DSF, DADS, ServiceConseil
+from .serializers import ServiceConseilSerializer, DSFSerializer, DADSSerializer,  PaysSerializer, FormeJuridiqueSerializer,EtatSalaireSerializer, ServiceSerializer, InformationsAutresSerializer,FicheVersementSpontaneIRPPSerializer, BalanceSixColonneSYSCohadaSerializer, FicheVersementAccompteISSerializer, FicheVerSementTVASerializer,DirigeantsSerializer,FicheEffectifSerializer,MembreConseilSerializer, FicheFiscaleSerializer,ControleEntrepriseSerializer, IdentificationSerializer, RegimeFiscaleSerializer
+from .models import DSF, DADS, ServiceConseil, Pays, FormeJuridique,EtatSalaire, Service, InformationsAutres,FicheVersementSpontaneIRPP, BalanceSixColonneSYSCohada, FicheVersementAccompteIS, FicheVerSementTVA,Dirigeants,FicheEffectif,MembreConseil, FicheFiscale,ControleEntreprise, Identification, RegimeFiscale
 from rest_framework import routers, serializers, viewsets
 # Create your views here.
 
@@ -15,3 +15,67 @@ class DADSViewSet(viewsets.ModelViewSet):
 class ServiceConseilViewSet(viewsets.ModelViewSet):
     queryset = ServiceConseil.objects.all()
     serializer_class = ServiceConseilSerializer
+
+class PaysViewSet(viewsets.ModelViewSet):
+    queryset = Pays.objects.all()
+    serializer_class = PaysSerializer
+
+class FormeJuridiqueViewSet(viewsets.ModelViewSet):
+    queryset = FormeJuridique.objects.all()
+    serializer_class = FormeJuridiqueSerializer
+
+class EtatSalaireViewSet(viewsets.ModelViewSet):
+    queryset = EtatSalaire.objects.all()
+    serializer_class = EtatSalaireSerializer
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class InformationsAutresViewSet(viewsets.ModelViewSet):
+    queryset = InformationsAutres.objects.all()
+    serializer_class = InformationsAutresSerializer
+
+class FicheVersementSpontaneIRPPViewSet(viewsets.ModelViewSet):
+    queryset = FicheVersementSpontaneIRPP.objects.all()
+    serializer_class = FicheVersementSpontaneIRPPSerializer
+
+class BalanceSixColonneSYSCohadaViewSet(viewsets.ModelViewSet):
+    queryset = BalanceSixColonneSYSCohada.objects.all()
+    serializer_class = BalanceSixColonneSYSCohadaSerializer
+
+class FicheVersementAccompteISViewSet(viewsets.ModelViewSet):
+    queryset = FicheVersementAccompteIS.objects.all()
+    serializer_class = FicheVersementAccompteISSerializer
+
+class FicheVerSementTVAViewSet(viewsets.ModelViewSet):
+    queryset = FicheVerSementTVA.objects.all()
+    serializer_class = FicheVerSementTVASerializer
+
+class DirigeantsViewSet(viewsets.ModelViewSet):
+    queryset = Dirigeants.objects.all()
+    serializer_class = DirigeantsSerializer
+
+class FicheEffectifViewSet(viewsets.ModelViewSet):
+    queryset = FicheEffectif.objects.all()
+    serializer_class = FicheEffectifSerializer
+
+class MembreConseilViewSet(viewsets.ModelViewSet):
+    queryset = MembreConseil.objects.all()
+    serializer_class = MembreConseilSerializer
+
+class FicheFiscaleViewSet(viewsets.ModelViewSet):
+    queryset = FicheFiscale.objects.all()
+    serializer_class = FicheFiscaleSerializer
+
+class ControleEntrepriseViewSet(viewsets.ModelViewSet):
+    queryset = ControleEntreprise.objects.all()
+    serializer_class = ControleEntrepriseSerializer
+
+class IdentificationViewSet(viewsets.ModelViewSet):
+    queryset = Identification.objects.all()
+    serializer_class = IdentificationSerializer
+
+class RegimeFiscaleViewSet(viewsets.ModelViewSet):
+    queryset = RegimeFiscale.objects.all()
+    serializer_class = RegimeFiscaleSerializer
